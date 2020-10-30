57b8aac9654a4af62aa663c5173abd50921c7905
Ben <bigriffin94@outlook.com>
2020-10-29 20:15:39 -0700

    Working on Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 // COMPONENTS \\
-import Nav from "./components/nav_bar/Nav";
 import About from "./components/about_me/about";
+import { BrowserRouter as Router } from 'react-router-dom';
+import NewNav from './components/newNav/newNav';
 // import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <div className="App">
-    <Nav />
-    <About />
+      <Router>
+        <NewNav />
+      </Router> 
+      <About />
     </div>
   );
 }
---
+++ src/components/newNav/newNav.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './newNav.css';
+
+function NewNav() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+// prevent defaults on rendering
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            ben griffin
+            {/* <i className="fas fa-microphone-alt"></i> */}
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NewNav;
