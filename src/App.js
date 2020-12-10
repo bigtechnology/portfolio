192518492d9c019417aeffa87e8144659c1e7606
Ben <bigriffin94@outlook.com>
2020-12-10 15:36:49 -0800

    Footer Complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
 // COMPONENTS \\
 import About from "./components/about_me/about";
-import { BrowserRouter as Router } from 'react-router-dom';
-import NewNav from './components/newNav/newNav';
-// import Footer from "./components/footer/footer";
+import Footer from "./components/footer/footer";
+import NewNav from "./components/newNav/newNav";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <NewNav />
-      </Router> 
+      <NewNav />
       <About />
+      <Footer />
     </div>
   );
 }
--- src/components/about_me/about.js
+++ src/components/about_me/about.js
@@ -9,17 +9,17 @@ function About() {
     <div className="about-wrapper">
       <div className="about-content">
         <div className="left-content">
-          <h1 className="profile">BEN GRIFFIN</h1>
+          <h1 className="header-name">BEN GRIFFIN</h1>
           <h4 className="profile-h4">front end engineer</h4>
         </div>
 
         <div className="middle-content">
-          <h1 className="intro-title">about me</h1>
-          <p className="about-para">
+          <h1 className="middle-title">about me</h1>
+          <p className="middle-para">
             I am a freelance web developer based out of San Francisco, CA. I
-            enjoy solving problems and making life easier through technology.
-            When I'm not coding, you'll find me cooking, gardening or working
-            out.
+            enjoy solving problems and making peopl'es lives easier through
+            technology. When I'm not coding, you'll find me cooking, reading or
+            exercising.
           </p>
         </div>
 
