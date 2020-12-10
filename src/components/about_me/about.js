34bfc8ade85e9f0aa4e4e1f27733522710e1828b
Ben <bigriffin94@outlook.com>
2020-12-10 14:05:26 -0800

    Images and correct sites restored

--- src/components/about_me/about.js
+++ src/components/about_me/about.js
@@ -1,41 +1,36 @@
 import React from "react";
-import "./about.css"
-import "../projects/projects.css"
+import "./about.css";
+import "../projects/projects.css";
 
 import Projects from "../projects/projects";
 
 function About() {
-    return (
-        <div className="about-wrapper">
-
-            <div className="about-content">
-
-            <div className="left-content">
-                <h1 className="profile">CRISTIAN GARCIA</h1>
-                <h4 className="profile-h4" >NASM CERTIFIED PERSONAL TRAINER</h4>
-            </div>
-
-                <div className="middle-content">
-                    <h1 className="intro-title"
-                    >about me</h1>
-                    <h4 className="about-h4"
-                    > I am a freelance NASM certified Personal Trainer.</h4>
-                    <p className="about-para">
-                    I enjoy helping others achieve their goals through fitness. 
-                    My goal is to help others discover the power that hard work, dedication and persistance can achieve. 
-                    </p>
-                </div>
-
-            <div className="right-content">
-            </div>
-
-            </div>
-
-            <div className="project-wrapper">
-            <Projects/>
-            </div>
+  return (
+    <div className="about-wrapper">
+      <div className="about-content">
+        <div className="left-content">
+          <h1 className="profile">BEN GRIFFIN</h1>
+          <h4 className="profile-h4">front end engineer</h4>
         </div>
-    )
+
+        <div className="middle-content">
+          <h1 className="intro-title">about me</h1>
+          <p className="about-para">
+            I am a freelance web developer based out of San Francisco, CA. I
+            enjoy solving problems and making life easier through technology.
+            When I'm not coding, you'll find me cooking, gardening or working
+            out.
+          </p>
+        </div>
+
+        <div className="right-content"></div>
+      </div>
+
+      <div className="project-wrapper">
+        <Projects />
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,43 +1,35 @@
 import React from "react";
 
 function Projects() {
-    return (
-<div className="projects-wrapper">
-
-<div className="projects">
-    <a className="project p1" href="#">
-        <div className="view">
-<<<<<<< HEAD
-            {/* view */}
-        </div>
-    </a>
-    <a href="https://cristian-garcia-official.vercel.app/" 
-    target="blank"className="project p3">
-        <div className="view">
-            {/* view */}
-=======
-        </div>
-    </a>
-    <a href="#" 
-     rel="noopener noreferrer"
-    className="project p3">
-        <div className="view">
->>>>>>> b45235a8e47e2ec402ee105f069e10ae22d2da5f
-        </div>
+  return (
+    <div className="projects-wrapper">
+      <div className="projects">
+        <a
+          className="project p1"
+          target="blank"
+          href="https://contest.storysquad.app/"
+        >
+          <div className="view">{/* view */}</div>
         </a>
-    <a className="project p2"
-     rel="noopener noreferrer"
-     href="#">
-        <div className="view">
-<<<<<<< HEAD
-            {/* view */}
-=======
->>>>>>> b45235a8e47e2ec402ee105f069e10ae22d2da5f
-        </div>
-    </a>
-</div>
-</div>
-    )
+
+        <a
+          href="https://cristian-garcia-official.vercel.app/"
+          target="blank"
+          className="project p2"
+        >
+          <div className="view">{/* view */}</div>
+        </a>
+
+        <a
+          className="project p3"
+          target="blank"
+          href="https://www.foundergrants.com/"
+        >
+          <div className="view">{/* view */}</div>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
