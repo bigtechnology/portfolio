2d6373f01d82e6f42be3561c67a8909e451bd0b7
B-Griffinn <bigriffin94@outlook.com>
2021-04-25 13:41:22 -0700

    About indexing

--- src/components/common/About/about.js
+++ src/components/common/About/about.js
@@ -7,10 +7,18 @@ const About = () => {
 
       <div className="about-content">
         <h2>Full Stack Web Developer</h2>
+
+        <div className="avatar-wrapper">
+          <div className="avatar-img"></div>
+        </div>
+
         <p>I am a Full Stack Web Developer based out of San Francisco, CA. I
             enjoy solving hard problems and making people's lives easier through
             technology. When I'm not coding, you'll find me cooking, reading or
             exercising.</p>
+        <p>
+        I design and code beautifully simple things, and I love what I do.
+        </p>
       </div>
     </div>
   );
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,3 +2,5 @@ export {About} from './About';
 export {Header} from './Header';
 export {Footer} from './Footer';
 export {Projects} from './Projects';
+export {Input} from './Input';
+export {ContactForm} from './Contact';
