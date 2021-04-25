70f524e4ac34aa7237509476f1519a8ca94b040c
B-Griffinn <bigriffin94@outlook.com>
2021-04-25 13:40:24 -0700

    Added Route to contact page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router';
 
 // COMPONENTS \\
 import { Footer, Header } from "./components/common";
+import { ContactPage } from "./components/pages";
 import { LandingPage } from "./components/pages/LandingPage";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
     <Switch>
       <Route exact path="/" component={LandingPage} />
+      <Route exact path="/contact" component={ContactPage} />
     </Switch>
     <Footer />
   </div>
