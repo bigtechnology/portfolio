8b5d6f57fa1a0afa6a937f83922f635b16839f06
B-Griffinn <bigriffin94@outlook.com>
2021-04-26 15:16:50 -0700

    Projects added to laning page

--- src/components/common/Projects/projects.js
+++ src/components/common/Projects/projects.js
@@ -1,35 +1,54 @@
-import React from "react";
+import React from 'react';
 
-function Projects() {
+const Projects = () => {
+
   return (
-    <div className="projects-wrapper">
-      <div className="projects">
-        <a
-          className="project p1"
-          target="blank"
-          href="https://contest.storysquad.app/"
-        >
-          <div className="view">{/* view */}</div>
-        </a>
+    <div className="projects-container">
+      <div className="header">
+        <h2>Projects</h2>
+      </div>
 
-        <a
-          href="https://cristian-garcia-official.vercel.app/"
-          target="blank"
-          className="project p2"
-        >
-          <div className="view">{/* view */}</div>
-        </a>
+      <div className="projects-content">
 
-        <a
-          className="project p3"
-          target="blank"
-          href="https://www.foundergrants.com/"
-        >
-          <div className="view">{/* view */}</div>
-        </a>
+        {/* <div className="video-wrapper">
+          <div title="" className="item-1"></div>
+        </div> */}
+
+        <div className="images-wrapper">
+          {/* <div className="header">
+            <h2>Testimonials</h2>
+          </div> */}
+          
+          <a className="item-1" href="https://contest.storysquad.app/" target="_blank" rel="noopener noreferrer">
+          <div title="" ></div>
+          </a>
+
+          <a className="item-2" href="https://contest.storysquad.app/" target="_blank" rel="noopener noreferrer">
+          <div title="" ></div>
+          </a>
+
+          <a className="item-3" href="https://blissful-lalande-b962e6.netlify.app/" target="_blank" rel="noopener noreferrer">
+          <div title="" ></div>
+          </a>
+
+          {/* <div title="" className="item-4"></div>
+          <div title="" className="item-5"></div>
+          <div title="" className="item-6"></div> */}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Projects;
+
+
+// const onClick = (item) => {
+//   if (item === 'item1') {
+//     window.open('https://contest.storysquad.app/')
+//   } else if (item === 'item2') {
+//     window.open('https://rumble.storysquad.app/')
+//   } else if (item === 'item3') {
+//     window.open('https://blissful-lalande-b962e6.netlify.app/')
+//   }
+// }
